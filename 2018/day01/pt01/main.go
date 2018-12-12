ae030cf34913c0b7bfb735a094d58f1aeba60a85
Daved <daved@codemodus.com>
2018-12-11 17:28:01 -0800

    Add d01p01 code.

---
+++ 2018/day01/pt01/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	f, err := os.Open("input")
+	trip(err)
+
+	sc := bufio.NewScanner(f)
+	var a int
+
+	for sc.Scan() {
+		c, err := strconv.Atoi(sc.Text())
+		trip(err)
+		a += c
+	}
+	trip(sc.Err())
+
+	fmt.Println(a)
+}
+
+func trip(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
