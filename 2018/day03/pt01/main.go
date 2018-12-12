74d88cbcd7d74abfc51e75574d848b06be0f9a75
Daved <daved@codemodus.com>
2018-12-11 21:05:07 -0800

    Add d03p01 code.

---
+++ 2018/day03/pt01/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var (
+		size = 1000
+	)
+
+	f, err := os.Open("input")
+	trip(err)
+	defer f.Close() //nolint
+
+	sc := bufio.NewScanner(f)
+	fab := makeFabric(size, size)
+
+	for sc.Scan() {
+		c := makeClaim(sc.Text())
+		fab.applyClaim(c)
+	}
+	trip(sc.Err())
+
+	fmt.Println(fab.overClaimed())
+}
+
+type claim struct {
+	id   int
+	x, y int
+	w, h int
+}
+
+func makeClaim(s string) claim {
+	ss := strings.Split(s, "@")
+	a := strings.TrimSpace(ss[0][1:])
+
+	ss = strings.Split(ss[1], ":")
+	b := strings.TrimSpace(ss[0])
+	c := strings.TrimSpace(ss[1])
+
+	id, err := strconv.Atoi(a)
+	trip(err)
+
+	ss = strings.Split(b, ",")
+	x, err := strconv.Atoi(strings.TrimSpace(ss[0]))
+	trip(err)
+	y, err := strconv.Atoi(strings.TrimSpace(ss[1]))
+	trip(err)
+
+	ss = strings.Split(c, "x")
+	w, err := strconv.Atoi(strings.TrimSpace(ss[0]))
+	trip(err)
+	h, err := strconv.Atoi(strings.TrimSpace(ss[1]))
+	trip(err)
+
+	return claim{
+		id: id,
+		x:  x,
+		y:  y,
+		w:  w,
+		h:  h,
+	}
+}
+
+type fabric [][]int
+
+func makeFabric(x, y int) fabric {
+	f := make([][]int, x)
+
+	for i := range f {
+		f[i] = make([]int, y)
+	}
+
+	return f
+}
+
+func (f fabric) applyClaim(c claim) {
+	for x := c.x; x < len(f) && x < c.x+c.w; x++ {
+		for y := c.y; y < len(f[x]) && y < c.y+c.h; y++ {
+			f[x][y]++
+		}
+	}
+}
+
+func (f fabric) overClaimed() int {
+	var a int
+
+	for i := range f {
+		for _, v := range f[i] {
+			if v > 1 {
+				a++
+			}
+		}
+	}
+
+	return a
+}
+
+func trip(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
