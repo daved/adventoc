766d8f84de7d2b7f6ec2fdbcb932d3a87a5a2474
Daved <daved@codemodus.com>
2018-12-12 12:05:51 -0800

    Add func sigs to d01p01 elm code.

--- 2018/elm/day01/pt01/src/Main.elm
+++ 2018/elm/day01/pt01/src/Main.elm
@@ -3,9 +3,11 @@ module Main exposing (result)
 import Input exposing (data)
 
 
+lines : List String
 lines =
     String.split "\n" data
 
 
+result : Int
 result =
     List.filterMap String.toInt lines |> List.sum
