b831052e10d4e0a9a95e44d40d5cafbd072d51a8
Daved <daved@codemodus.com>
2018-12-11 18:10:01 -0800

    Add d01p02 code.

---
+++ 2018/day01/pt02/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	f, err := os.Open("input")
+	trip(err)
+
+	var a int
+	m := make(map[int]struct{})
+	var r *int
+
+	for {
+		sc := bufio.NewScanner(f)
+
+		for sc.Scan() {
+			c, err := strconv.Atoi(sc.Text())
+			trip(err)
+			a += c
+
+			if _, ok := m[a]; ok {
+				r = &a
+				break
+			}
+			m[a] = struct{}{}
+		}
+		trip(sc.Err())
+
+		if r != nil {
+			break
+		}
+		_, _ = f.Seek(0, 0) //nolint
+	}
+
+	fmt.Println(*r)
+}
+
+func trip(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
