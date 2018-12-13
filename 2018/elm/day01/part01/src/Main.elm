0f56caf0d183f01ad60d8255cf40371e70674806
Daved <daved@codemodus.com>
2018-12-12 19:33:47 -0800

    Refactor d01p01 elm code.

--- 2018/elm/day01/part01/src/Main.elm
+++ 2018/elm/day01/part01/src/Main.elm
@@ -3,11 +3,8 @@ module Main exposing (result)
 import Input exposing (data)
 
 
-lines : List String
-lines =
-    String.split "\n" data
-
-
 result : Int
 result =
-    List.filterMap String.toInt lines |> List.sum
+    String.split "\n" data
+        |> List.filterMap String.toInt
+        |> List.sum
